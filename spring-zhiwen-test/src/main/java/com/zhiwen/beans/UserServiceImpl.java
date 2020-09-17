eedb890797204e55001b3c3225a62a8df2eca10b
zhiwen <wangzhiwen4@guazi.com>
2020-09-17 09:40:47 +0800

    新建 demo

---
+++ spring-zhiwen-test/src/main/java/com/zhiwen/beans/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package com.zhiwen.beans;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author wangzhiwen
+ * @version 1.0
+ * @description
+ * @date 2020/9/17 08:55
+ **/
+@Service
+public class UserServiceImpl {
+	public void sayHi(){
+		System.out.println("Hello Spring！");
+	}
+}
